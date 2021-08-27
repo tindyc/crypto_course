9bf2bd56e765fa7e0979bb8a6bfdf8a097ede607
tindyc <tindyc@bath.edu>
2021-08-27 00:43:49 +0000

    Fix database and deploy to heroku

--- crypto_course/settings.py
+++ crypto_course/settings.py
@@ -125,12 +125,17 @@ WSGI_APPLICATION = 'crypto_course.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
