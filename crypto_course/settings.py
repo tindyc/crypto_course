2a553f7f205c945eac22608f39cb2795805bffd1
tindyc <57362474+tindyc@users.noreply.github.com>
2023-03-16 19:50:20 +0000

    prepare for render

--- crypto_course/settings.py
+++ crypto_course/settings.py
@@ -29,6 +29,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cryptocourse.herokuapp.com', 'localhost', '127.0.0.1']
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
