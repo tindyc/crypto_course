5bb7ff3fe48602a686bed7032a240ef83c8ad143
tindyc <57362474+tindyc@users.noreply.github.com>
2023-03-16 20:16:22 +0000

    update media url and root

--- crypto_course/settings.py
+++ crypto_course/settings.py
@@ -184,6 +184,9 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'tindyc-crypto-course'
